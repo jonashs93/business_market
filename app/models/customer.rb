c654ddb3e72815946e1587fa19b10ab402f1174f
Jonas Silva <jonas_jhs@hotmail.com>
2020-01-29 22:47:10 -0300

    implements model customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,8 @@
 class Customer < ApplicationRecord
+
+  # --- Validations ---
+
+  validates :name, length: { maximum: 100 }
+  validates :cpf, presence: true, uniqueness: true, length: { is: 14 }
+  validates :email, email_format: true, length: { maximum: 100 }
 end
