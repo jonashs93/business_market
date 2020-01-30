7b5606ecdebbd81160c24795e50434a139084b16
Jonas Silva <jonas_jhs@hotmail.com>
2020-01-30 11:08:43 -0300

    tests to phone model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_29_235556) do
+ActiveRecord::Schema.define(version: 2020_01_30_021314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2020_01_29_235556) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
+  end
+
+  create_table "phone_models", force: :cascade do |t|
+    t.string "name", limit: 100, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
